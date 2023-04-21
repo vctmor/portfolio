0411157dd16ba50545302ebbc48b578f695e0be0
victor_moreira <victormoreira@vivaldi.net>
2023-04-20 21:02:14 -0300

    preparando o ambiente, resolvendo bugs

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+import Head from 'next/head'
+
+//import { Inter } from 'next/font/google'
+
+
+export default function Home() {
+  return (
+    <h1>Teste</h1>
+
+  )
+}
